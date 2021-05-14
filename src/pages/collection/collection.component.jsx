2489bbaa0ee2ba48b4a488c6a3ec9d3c49f85b87
ntospa21 <tommyaek@gmail.com>
2021-05-14 11:39:52 +0300

    updated the app component, sign in component and sign up component with hooks

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,6 +5,7 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
+
 import {
   CollectionPageContainer,
   CollectionTitle,
@@ -12,6 +13,9 @@ import {
 } from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
+
+
+
   const { title, items } = collection;
   return (
     <CollectionPageContainer>
