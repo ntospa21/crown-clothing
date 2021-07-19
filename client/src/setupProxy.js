2c2fcf434a56a4ee93e6f11b63e2335dc7167475
ntospa21 <tommyaek@gmail.com>
2021-07-19 13:20:46 +0300

    separate folders

---
+++ client/src/setupProxy.js
@@ -0,0 +1,7 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+     
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware('/payment', { target: 'http://localhost:5000' })
+  );
+};
